d939c309384c122243029b19efa869cb49b36494
Patrick Altman <paltman@gmail.com>
2011-02-19 13:46:22 -0600

    updated ModelAdmin to be in line with change in manager

--- logicaldelete/admin.py
+++ logicaldelete/admin.py
@@ -2,11 +2,16 @@ from django.contrib import admin
 
 
 class ModelAdmin(admin.ModelAdmin):
-    list_display = ('id', '__unicode__', 'active')
-    list_display_filter = ('active',)
-
+    """
+    A base model admin to use in providing access to to logically deleted
+    objects.
+    """
+    
+    list_display = ("id", "__unicode__", "active")
+    list_display_filter = ("active",)
+    
     def queryset(self, request):
-        qs = self.model._default_manager.everything()
+        qs = self.model._default_manager.all_with_deleted()
         ordering = self.ordering or ()
         if ordering:
             qs = qs.order_by(*ordering)
