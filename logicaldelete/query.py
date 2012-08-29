41b07d232b7b3344fe81efb0c4c20568e2b7500b
Patrick Altman <paltman@gmail.com>
2012-08-29 08:10:52 -0500

    Add missing query.py to repo

---
+++ logicaldelete/query.py
@@ -0,0 +1,12 @@
+from django.db.models.query import QuerySet
+
+
+class LogicalDeleteQuerySet(QuerySet):
+    
+    def delete(self):
+        assert self.query.can_filter(), \
+                "Cannot use 'limit' or 'offset' with delete."
+        for obj in self.all():
+            obj.delete()
+        self._result_cache = None
+    delete.alters_data = True
