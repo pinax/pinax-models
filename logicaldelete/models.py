17e756f9ed3cfa9e7b11376e6899802812dc68ee
Angel Velasquez <angvp@archlinux.org>
2013-08-30 21:12:56 -0300

    Add timezone support

--- logicaldelete/models.py
+++ logicaldelete/models.py
@@ -1,4 +1,7 @@
-import datetime
+try:
+    from django.utils import timezone
+except ImportError:
+    from datetime import datetime as timezone
 
 from django.db import models
 
@@ -10,20 +13,20 @@ class Model(models.Model):
     This base model provides date fields and functionality to enable logical
     delete functionality in derived models.
     """
-    
-    date_created  = models.DateTimeField(default=datetime.datetime.now)
-    date_modified = models.DateTimeField(default=datetime.datetime.now)
+
+    date_created  = models.DateTimeField(default=timezone.now)
+    date_modified = models.DateTimeField(default=timezone.now)
     date_removed  = models.DateTimeField(null=True, blank=True)
-    
+
     objects = managers.LogicalDeletedManager()
-    
+
     def active(self):
-        return self.date_removed == None
+        return self.date_removed is None
     active.boolean = True
-    
+
     def delete(self):
-        self.date_removed = datetime.datetime.now()
+        self.date_removed = timezone.now()
         self.save()
-    
+
     class Meta:
         abstract = True
